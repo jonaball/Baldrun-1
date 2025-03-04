dd41ecc997dd5dc7e853b2932588d220b8bb45f7
Jonathan Tønseth <66827544+jonaball@users.noreply.github.com>
2025-03-04 12:02:10 +0100

    Kan lukke spillet nå

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sys
 
 pg.init()
 
@@ -6,17 +7,27 @@ pg.init()
 fps = 60 # Spill fps
 tikk = pg.time.Clock()
 
+# Farger
+SVART = (0, 0, 0)
+HVIT = (255, 255 , 255)
+RØD = (255, 0, 0)
+GRØNN = (0, 255, 0)
+BLÅ = (0, 0, 255)
+
 # Skjerminstillinger
 SKJERM_HØYDE = 800
 SKJERM_BREDDE = 800
-# Farger
-
 
 # Definer skjerm
-pg.display.set_mode((SKJERM_BREDDE, SKJERM_HØYDE))
-pg.display.fill(255,255,255)
+SKJERM = pg.display.set_mode((SKJERM_BREDDE, SKJERM_HØYDE))
+SKJERM.fill(HVIT)
 
 # Spilløkke
 running = True
 while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+    
     pg.display.flip()
