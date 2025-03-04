b124eebff35396c5d2e1cb7da2ee879b71747bf7
Jonathan Tønseth <66827544+jonaball@users.noreply.github.com>
2025-03-04 11:09:44 +0100

    Instillinger

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame as pg
 
 pg.init()
 
-# Skjermdimensjoner
+# Skjerminstillinger
 SKJERM_HØYDE = 800
 SKJERM_BREDDE = 800
+    # Farger
+SVART = (0,0,0)
+HVIT = (255,255,255)
+RØD = (255,0,0)
+GRØNN = (0,255,0)
+BLÅ = (0,0,255)
 
 pg.display.set_mode((SKJERM_BREDDE, SKJERM_HØYDE))
 pg.display.flip()
