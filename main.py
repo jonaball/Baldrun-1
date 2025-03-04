c12dc0eb5cdc2f79456e381b97a38f144b8dcc2c
Jonathan Tønseth <66827544+jonaball@users.noreply.github.com>
2025-03-04 20:01:49 +0100

    DUDE stages

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame as pg
 import sys
-import objekter as obj
 
 pg.init()
 
@@ -16,17 +15,33 @@ GRØNN = (0, 255, 0)
 BLÅ = (0, 0, 255)
 
 # Skjerminstillinger
-SKJERM_HØYDE = 800
-SKJERM_BREDDE = 800
+SKJERM_HØYDE = 720
+SKJERM_BREDDE = 1080
 
 # Definer skjerm
 SKJERM = pg.display.set_mode((SKJERM_BREDDE, SKJERM_HØYDE))
-SKJERM.fill(BLÅ)
+SKJERM.fill(SVART)
 
-SKJERM_CENTER = (SKJERM_BREDDE//2, SKJERM_HØYDE//2)
+CENTER_X = SKJERM_BREDDE//2
+CENTER_Y = SKJERM_HØYDE//2
 
 # Objekter
-obj.Dude("Prosjekt\Pygame_spill\Sprites\Head_hair0.png", SKJERM_CENTER)
+class Dude():
+    """
+    Lag en dude
+    """
+    def __init__(self, image, x, y):
+        self.image = pg.image.load(image)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.x = x - self.width//2
+        self.y = y - self.height//2
+
+    def render(self, skjerm):
+        skjerm.blit(self.image, (self.x, self.y))
+
+HAIRSTAGE = "Pygame_spill\Sprites\Head_hair0.png"
+DUDE = Dude(HAIRSTAGE, CENTER_X, CENTER_Y)
 
 # Spilløkke
 running = True
@@ -36,6 +51,16 @@ while running:
             running = False
             pg.quit()
             sys.exit()
-    
+        if event.type == pg.KEYDOWN:
+            if event.type == pg.K_0:
+                HAIRSTAGE = "Pygame_spill\Sprites\Head_hair0.png"
+            if event.type == pg.K_1:
+                HAIRSTAGE = "Pygame_spill\Sprites\Head_hair1.png"
+            if event.type == pg.K_2:
+                HAIRSTAGE = "Pygame_spill\Sprites\Head_hair2.png"
+            if event.type == pg.K_3:
+                HAIRSTAGE = "Pygame_spill\Sprites\Head_hair3.png" 
+
+    DUDE.render(SKJERM)
     pg.display.flip()
     tikk.tick(fps)
--- objekter.py
+++ objekter.py
@@ -1,13 +1,1 @@
-import pygame as pg
 
-class Dude():
-    """
-    Lag en dude
-    """
-    def __init__(self, image, x, y):
-        self.image = pg.image.load(image)
-        self.x = x
-        self.y = y
-
-    def render(self, skjerm):
-        skjerm.blit(self.image, (self.x, self.y))
