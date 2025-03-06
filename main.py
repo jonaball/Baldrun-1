68cc85c64f1d503b2c1a39e45e18975c1c880629
Jonathan Tønseth <66827544+jonaball@users.noreply.github.com>
2025-03-06 11:43:54 +0100

    Rotation

--- main.py
+++ main.py
@@ -43,31 +43,39 @@ class Dude():
     Lag en dude
     """
     def __init__(self, x, y, scale):
-        self.hair0 = pg.image.load("Prosjekt-Pygame\Sprites\Head_hair0.png")
+        self.hair0 = pg.image.load("Prosjekt-Pygame\Sprites\Head_hair0.png") # Frisyrer
         self.hair1 = pg.image.load("Prosjekt-Pygame\Sprites\Head_hair1.png")
         self.hair2 = pg.image.load("Prosjekt-Pygame\Sprites\Head_hair2.png")
         self.hair3 = pg.image.load("Prosjekt-Pygame\Sprites\Head_hair3.png")
-
         self.frisyre = self.hair0 # Frisyren dude starter med
 
         self.scale = scale # Størrelsen til dude (Definert i -- Objektinstillinger --)
-        
         self.width = self.scale[0]
         self.height = self.scale[1]
         self.x = x - self.width//2
         self.y = y - self.height//2
+        
+        self.opp = 0
+        self.venstre = 90
+        self.ned = 180
+        self.høyre = -90
+        self.retning = self.opp # Startretning er alltid opp
 
         self.oppdater_frisyre() # Sørg for at frisyren er oppdatert til den nye størrelsen
+        self.oppdater_retning() # Sørg for at duden peker riktig vei
 
     def oppdater_frisyre(self):
         self.skalert_dude = pg.transform.scale(self.frisyre, self.scale) # Skalerer dude til riktig størrelse
+    
+    def oppdater_retning(self):
+        self.skalert_dude = pg.transform.rotate(self.skalert_dude, self.retning) # Roterer dude i riktig retning
 
     def tegn(self, skjerm):
         skjerm.blit(self.skalert_dude, (self.x, self.y)) # Viser den nye, og skalerte, duden
 
 class Map():
     """
-    Lag mappet
+    Map stuff
     """
     def __init__(self, scale):
         self.tilesize = scale
@@ -110,12 +118,17 @@ while running:
     # Bevegelse (flytter map/bakgrunn)
     if taster[pg.K_w]:
         MAP.offset_y += 500/fps
+        DUDE.retning = DUDE.opp
     if taster[pg.K_a]:
         MAP.offset_x += 500/fps 
+        DUDE.retning = DUDE.venstre
     if taster[pg.K_s]:
         MAP.offset_y -= 500/fps
+        DUDE.retning = DUDE.ned
     if taster[pg.K_d]:
         MAP.offset_x -= 500/fps
+        DUDE.retning = DUDE.høyre
+    DUDE.oppdater_retning() # VIKTIG! Passer på at duden peker i riktig retning når den byttes
 
     # -- Vis skjermobjekter --
     SKJERM.fill(BG_FARGE)
